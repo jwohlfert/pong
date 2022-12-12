5601b341ba49a82957c535958aa41987bc810539
John Wohlfert <jwohlfert@nexient.com>
2022-12-12 11:33:59 -0500

    improved paddle class based on instructions from video

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-right_paddle = Paddle(paddle_side="right")
-left_paddle = Paddle(paddle_side="left")
+LEFT_POSITION = (-350, 0)
+RIGHT_POSITION = (350, 0)
+
+right_paddle = Paddle(paddle_coordinates=RIGHT_POSITION)
+left_paddle = Paddle(paddle_coordinates=LEFT_POSITION)
 
 screen.listen()
 screen.onkey(key="Up", fun=right_paddle.move_up)
 screen.onkey(key="Down", fun=right_paddle.move_down)
+screen.onkey(key="w", fun=left_paddle.move_up)
+screen.onkey(key="s", fun=left_paddle.move_down)
 
 game_is_on = True
 while game_is_on:
--- paddle.py
+++ paddle.py
@@ -1,32 +1,19 @@
 from turtle import Turtle
 
-LEFT_POSITION = (-350, 0)
-RIGHT_POSITION = (350, 0)
-
 
 class Paddle(Turtle):
-    def __init__(self, paddle_side):
+    def __init__(self, paddle_coordinates):
         super().__init__()
-        self.paddle = None
-        self.create_paddle(paddle_side)
-
-    def create_paddle(self, side):
-        self.paddle = Turtle()
-        self.paddle.shape("square")
-        self.paddle.turtlesize(stretch_wid=5, stretch_len=1)
-        self.paddle.color("white")
-        self.paddle.penup()
-        if side.lower() == "right":
-            self.paddle.goto(RIGHT_POSITION)
-        elif side.lower() == "left":
-            self.paddle.goto(LEFT_POSITION)
-        else:
-            raise ValueError(f"'{side}' is not a valid input. Valid inputs are 'left' or 'right'")
+        self.shape("square")
+        self.turtlesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(paddle_coordinates)
 
     def move_up(self):
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(x=self.paddle.xcor(), y=new_y)
+        new_y = self.ycor() + 20
+        self.goto(x=self.xcor(), y=new_y)
 
     def move_down(self):
-        new_y = self.paddle.ycor() - 20
-        self.paddle.goto(x=self.paddle.xcor(), y=new_y)
+        new_y = self.ycor() - 20
+        self.goto(x=self.xcor(), y=new_y)
