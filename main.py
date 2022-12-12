1ab612f61b58c992667bda9b84fdb632d9528648
John Wohlfert <jwohlfert@nexient.com>
2022-12-12 12:32:11 -0500

    game is nearly complete. all that's left is a win condition and corresponding method of the scoreboard

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import time
 from turtle import Screen
+
+from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.colormode(255)
@@ -11,8 +15,10 @@ screen.tracer(0)
 LEFT_POSITION = (-350, 0)
 RIGHT_POSITION = (350, 0)
 
+ball = Ball()
 right_paddle = Paddle(paddle_coordinates=RIGHT_POSITION)
 left_paddle = Paddle(paddle_coordinates=LEFT_POSITION)
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(key="Up", fun=right_paddle.move_up)
@@ -22,6 +28,25 @@ screen.onkey(key="s", fun=left_paddle.move_down)
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # detect ball hitting the top or bottom
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with right paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # detect ball going past the paddle
+    if ball.xcor() > 400:
+        ball.ball_reset()
+        scoreboard.l_point()
+
+    if ball.xcor() < -400:
+        ball.ball_reset()
+        scoreboard.r_point()
 
 screen.exitonclick()
